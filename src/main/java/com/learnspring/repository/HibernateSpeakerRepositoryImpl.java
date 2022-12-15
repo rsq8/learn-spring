9adc1709385946baeea8bfd12b48ea5457164dc4
Yusuf Dauda <yusuf.dauda@tesco.com>
2022-12-15 15:12:37 +0000

    added models and repository

--- src/main/java/com/learnspring/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/learnspring/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,2 +1,33 @@
-package com.learnspring.repository;public class HibernateSpeakerRepositoryImpl {
+package com.learnspring.repository;
+
+import com.learnspring.model.Speaker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Override
+    public List<Speaker> findAll(){
+        List<Speaker> speakers = new ArrayList<Speaker>();
+
+        Speaker first = new Speaker();
+        Speaker second = new Speaker();
+        Speaker third = new Speaker();
+
+        first.setFirstName("Yusuf");
+        first.setLastName("Dauda");
+
+        second.setFirstName("Aliaa");
+        second.setLastName("Dauda");
+
+        third.setFirstName("Zainab");
+        third.setLastName("Dauda");
+
+        speakers.add(first);
+        speakers.add(second);
+        speakers.add(third);
+
+        return speakers;
+    }
 }
--- src/main/java/com/learnspring/repository/SpeakerRepository.java
+++ src/main/java/com/learnspring/repository/SpeakerRepository.java
@@ -1,2 +1,9 @@
-package com.learnspring.repository;public interface SpeakerRepository {
+package com.learnspring.repository;
+
+import com.learnspring.model.Speaker;
+
+import java.util.List;
+
+public interface SpeakerRepository {
+    List<Speaker> findAll();
 }
