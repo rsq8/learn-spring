dc55dbcded400bfb51e927c65e79e4181d437606
Yusuf Dauda <yusuf.dauda@tesco.com>
2022-12-17 13:21:47 +0000

    beans configuration using xml

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,11 +1,18 @@
 import com.learnspring.service.SpeakerService;
 import com.learnspring.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import static java.lang.System.out;
 
 public class Application {
     public static void main(String[] args){
-        SpeakerService service = new SpeakerServiceImpl();
+
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        //SpeakerService service = new SpeakerServiceImpl();
+
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
         out.println(service.findAll().get(2).getFirstName());
     }
 }
